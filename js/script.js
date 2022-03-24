9d24df335c2ca48dab5ce83081c7ff927e82291d
Volodymyr <volodymyr.sekh.dev@gmail.com>
2022-03-25 01:28:29 +0200

    bug fix

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
     async function getResponse() {
-        let response = await fetch('http://localhost:8080/')
+        let response = await fetch('http://localhost:8080/',{mode:'no-cors'})
         let content = await response.json();
         let list = document.querySelector('.wrapper');
         let key;
-    
+       
+
         for(key in content) {
             list.innerHTML += `
                 <h1>User id: ${content[key].id}</h1>
