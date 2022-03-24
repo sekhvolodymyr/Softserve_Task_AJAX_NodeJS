563cf5a0b9b4484f77b6228c71095848ccd47dd4
Dmitry Yefimov <efimov.dmitry.33@gmail.com>
2022-03-24 10:46:43 +0200

    Client side

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    async function getResponse() {
+        let response = await fetch('js/users.json')
+        let content = await response.json();
+        let list = document.querySelector('.wrapper');
+        let key;
+    
+        for(key in content) {
+            list.innerHTML += `
+                <h1>User id: ${content[key].id}</h1>
+                <p>Name: ${content[key].first_name}</p>
+                <p>Lastname: ${content[key].last_name}</p>
+                <p>Email: ${content[key].email}</p>
+                <img src="${content[key].photo}" width="300">
+            `
+        }
+    }
+    getResponse();
+})
