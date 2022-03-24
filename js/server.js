32341534e686969ed319e44ee50b78f1ac4eb027
Volodymyr <volodymyr.sekh.dev@gmail.com>
2022-03-25 00:49:35 +0200

    update server

--- js/server.js
+++ js/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 http.createServer((req,res) => {
-    const readStream = fs.createReadStream('./users.json');
+    const readStream = fs.createReadStream('https://sekhvolodymyr.github.io/Softserve_Task_AJAX_NodeJS/js/users.json');
     res.writeHead(200,{'Content-type':'application/json'})
     readStream.pipe(res,'utf-8');
 }).listen(8080);
