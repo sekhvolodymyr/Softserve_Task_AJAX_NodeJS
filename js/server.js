d136dd90f52e3d8a8ee1ffcdb795bdacc3e11beb
Volodymyr <volodymyr.sekh.dev@gmail.com>
2022-03-25 12:24:33 +0200

    server bug fix with cors

--- js/server.js
+++ js/server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const fs = require('fs');
 http.createServer((req,res) => {
     const readStream = fs.createReadStream('./users.json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     res.writeHead(200,{'Content-type':'application/json'})
     readStream.pipe(res,'json');
 }).listen(3000);
